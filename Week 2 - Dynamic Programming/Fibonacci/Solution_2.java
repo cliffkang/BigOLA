b41e861ed402dbeb89e64ba97f93321006cbdb36
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-10-04 18:44:21 -0700

    Add fibonacci java solutions and problem statement

---
+++ Week 2 - Dynamic Programming/Fibonacci/Solution_2.java
@@ -0,0 +1,20 @@
+class Solution_2 {
+
+    long fibonacci(final int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        long a = 0;
+        long b = 1;
+        long c = -1;
+
+        for (int i = 2; i < n + 1; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return c;
+    }
+}
