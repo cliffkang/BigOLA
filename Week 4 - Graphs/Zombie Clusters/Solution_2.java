82a8b37e9ff357a0dec022215fc929ec6564a547
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-11-01 00:43:42 -0700

    Add zombie clusters java solutions

---
+++ Week 4 - Graphs/Zombie Clusters/Solution_2.java
@@ -0,0 +1,37 @@
+class Solution_2 {
+
+    int zombieClusters(final int[][] grid) {
+        final int lines = grid.length;
+        final int columns = lines > 0 ? grid[0].length : 0;
+
+        if (lines == 0 || columns == 0) {
+            return 0;
+        }
+
+        int result = 0;
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < columns; j++) {
+                result += zombieClustersHelper(grid, i, j);
+            }
+        }
+
+        return result;
+    }
+
+    private int zombieClustersHelper(final int[][] grid, final int line, final int column) {
+        if (line >= grid.length || column >= grid[0].length) {
+            return 0;
+        }
+
+        if (grid[line][column] == 0) {
+            return 0;
+        }
+
+        grid[line][column] = 0;
+
+        zombieClustersHelper(grid, line + 1, column);
+        zombieClustersHelper(grid, line, column + 1);
+
+        return 1;
+    }
+}
